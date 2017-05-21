7408405be3f9488370ecfdc8e84a4497c223480a
Tanveer Sehgal <tanveersehgal@gmail.com>
2017-05-22 00:31:10 +0700

    Add tests

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,4 +1,8 @@
 import { AngularE2eTestingPage } from './app.po';
+import { browser } from 'protractor';
+
+// List of items in the menu
+const menu = ['Espresso', 'Cappuccino', 'Latte', 'Mocha'];
 
 describe('angular-e2e-testing App', () => {
   let page: AngularE2eTestingPage;
@@ -7,8 +11,46 @@ describe('angular-e2e-testing App', () => {
     page = new AngularE2eTestingPage();
   });
 
-  it('should display message saying app works', () => {
+  it('should display banner content correctly', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+    expect(page.getBannerTitleText()).toEqual(`Angular's Coffee Shop`);
+    expect(page.getBannerText()).toEqual(`Welcome to Angular's Coffee Shop! It's a beautiful day isn't it?`);
+  });
+
+  it('should display coffee machine', () => {
+    page.navigateTo();
+    expect(page.getCoffeeMachine().isDisplayed).toBeTruthy();
+  });
+
+  it('should display all menu items correctly', () => {
+    page.navigateTo();
+
+    expect(page.getMenuCount()).toEqual(menu.length);
+    menu.map((item, key) => {
+      expect(page.getMenuItemAt(key)).toEqual(menu[key]);
+    });
+  });
+
+  it('should update status correctly when new order is placed', () => {
+    // Use this so that protractor doesn't wait for timeout before running assertions
+    browser.waitForAngularEnabled(false);
+
+    page.navigateTo();
+    page.placeOrder(1);
+
+    expect(page.getStatus()).toEqual(`Brewing you an amazing ${menu[1]}!!`);
+    expect(page.getCancelButton().isDisplayed()).toBeTruthy();
+
+    // Reset for other tests
+    browser.waitForAngularEnabled(true);
+  });
+
+  // Note: In this test, protractor will wait for the timeout to execute before it runs the assertions.
+  it('should show order complete 2 seconds after the order has been place', () => {
+    page.navigateTo();
+    page.placeOrder(1);
+
+    expect(page.getStatus()).toEqual('Your order is ready!!');
+    expect(page.getOrderReadyButton().isDisplayed()).toBeTruthy();
   });
 });
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -5,7 +5,39 @@ export class AngularE2eTestingPage {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('app-root h1')).getText();
+  getBannerTitleText() {
+    return element(by.css('#main-banner h1')).getText();
+  }
+
+  getBannerText() {
+    return element(by.css('#main-banner p')).getText();
+  }
+
+  getCoffeeMachine() {
+    return element(by.tagName('img'));
+  }
+
+  getMenuCount() {
+    return element.all(by.css('.coffee-type')).count();
+  }
+
+  getMenuItemAt(position) {
+    return element.all(by.css('.coffee-type')).get(position).getText();
+  }
+
+  placeOrder(position) {
+    element.all(by.css('.coffee-type')).get(position).click();
+  }
+
+  getStatus() {
+    return element(by.css('.well h4')).getText();
+  }
+
+  getCancelButton() {
+    return element(by.css('.btn-danger'));
+  }
+
+  getOrderReadyButton() {
+    return element(by.css('.btn-success'));
   }
 }
